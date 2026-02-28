f825c1ef7cb7ce3852e2fa20bf44428c7f8337a7
Harsh-Koundal <aharsh3039@gmail.com>
2026-02-28 08:58:36 -0800

    client setupt

---
+++ Client-Side/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" />
+                <button type="submit"
+                    onClick={()=>handleLogin()}>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
