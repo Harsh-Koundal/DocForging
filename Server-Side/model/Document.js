224ebdd265f317b5b8f0471d27cee2b7a18de5d7
Harsh-Koundal <aharsh3039@gmail.com>
2026-03-14 07:58:09 -0400

    Document schema

---
+++ Server-Side/model/Document.js
@@ -0,0 +1,73 @@
+import mongoose, { mongo } from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+    {
+        name :{
+            type:String,
+            required:true,
+        },
+
+        type:{
+            type:String,
+            enum : ["docx","pdf","txt","image","collab"],
+            required:true,
+        },
+
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+
+        collaborators:[
+            {
+                user:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"User",
+                },
+                role:{
+                    enum:["viewer","editor","owner"],
+                    default:"viewer",
+                }
+            }
+        ],
+        
+        fileUerl:{
+            type:String,
+        },
+
+        fileSize:{
+            type:Number,
+        },
+
+        storageProvider:{
+            type:String,
+            enum:["local","aws","cloudinary"],
+            default:"local",
+        },
+
+        folder:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Folder",
+        },
+
+        isStarred:{
+            type:Boolean,
+            default:false,
+        },
+
+        isDeleted:{
+            type:Boolean,
+            default:false,
+        },
+
+        lasrOpened:{
+            type:Date,
+        },
+    },
+    {
+        timestamps:true,
+    },
+)
+
+export default mongoose.model("Document",documentSchema);
