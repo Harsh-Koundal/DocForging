92de721a1bfb3ba68b7b920f139e64ac36096e88
Harsh-Koundal <aharsh3039@gmail.com>
2026-03-14 07:36:50 -0400

    file preview

---
+++ Client-Side/src/components/FilePreview.jsx
@@ -0,0 +1,13 @@
+export default function FilePreviewLines({ lineBg, accentBg }) {
+  return (
+    <div className="flex flex-col gap-1.5">
+      <div className={`h-1.5 rounded ${accentBg} w-1/2`} />
+      <div className={`h-1.5 rounded ${lineBg} w-4/5 mt-1`} />
+      <div className={`h-1.5 rounded ${lineBg}`} />
+      <div className={`h-1.5 rounded ${lineBg} w-2/3`} />
+      <div className={`h-1.5 rounded ${lineBg}`} />
+      <div className={`h-1.5 rounded ${lineBg} w-2/5`} />
+      <div className={`h-1.5 rounded ${lineBg} w-3/5`} />
+    </div>
+  );
+}
